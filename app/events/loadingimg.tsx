6555d5a4766bc71a7dcea618e8d5daee841b2722
Jacob <jacob@jacobdaniel.co.uk>
2025-07-17 17:47:07 +0100

    remove events

--- app/events/loadingimg.tsx
+++
@@ -1,5 +0,0 @@
-export default function Loading() {
-	return (
-		<div className="fill-container justify-center loading relative mb-2 block flex h-auto bg-gray-300 mb-5 w-[100px] h-auto"></div>
-	);
-}
