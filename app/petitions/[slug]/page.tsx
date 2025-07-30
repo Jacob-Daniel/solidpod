262693dc24dfd115680f4720b787b8e1d0cb96f9
Jacob <jacob@jacobdaniel.co.uk>
2025-07-30 16:15:32 +0100

    fix sig query petitions not petition

--- app/petitions/[slug]/page.tsx
+++ app/petitions/[slug]/page.tsx
@@ -44,7 +44,7 @@ async function fetchPetitionData(slug: string) {
 
 async function fetchSignatureData(slug: string) {
 	return getPagAPI<Signature[]>(
-		`/signatures?filters[$and][0][petition][slug][$eq]=${slug}&filters[$and][1][comment][$notNull]=true&populate=user&pagination[page]=1&pagination[pageSize]=2`,
+		`/signatures?filters[$and][0][petitions][slug][$eq]=${slug}&filters[$and][1][comment][$notNull]=true&populate=user&pagination[page]=1&pagination[pageSize]=2`,
 	);
 }
 
