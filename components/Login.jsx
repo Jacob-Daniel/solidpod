df5382adb5e8e82cf8b01f9c2c370a27543ff0b6
Jacob <jacob@jacobdaniel.co.uk>
2025-08-01 16:20:25 +0100

    add own solid server

--- components/Login.jsx
+++ components/Login.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import SolidLogin from "./SolidLogin";
 
 export default function SolidAuthFlow() {
-  const [solidServer] = useState("https://solidcommunity.net");
+  const [solidServer] = useState("https://gardenmap.jacobdaniel.co.uk");
   return (
     <div className="w-100 bg-blue-200 p-2">
       <SolidLogin solidServer={solidServer} />
