54de8b23e20ad56e0952885a6bb7941b20e006ef
Jacob <jacob@jacobdaniel.co.uk>
2025-09-17 18:20:15 +0100

    .env vars

--- app/solid/LoginButton.tsx
+++ app/solid/LoginButton.tsx
@@ -10,7 +10,7 @@ const LoginButton: FC = () => {
   const login = async () => {
     await session.login({
       redirectUrl: process.env.NEXT_PUBLIC_STRAPI_BASE_URL,
-      oidcIssuer: process.env.CSS_URL,
+      oidcIssuer: process.env.NEXT_PUBLIC_CSS_URL,
       clientName: "NKSJA App",
     });
   };
