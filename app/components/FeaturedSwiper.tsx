9f28d3c1bff94f91d623729f797cbec23ffdacc4
Jacob <jacob@jacobdaniel.co.uk>
2025-09-22 19:01:21 +0100

    remove slide

--- app/components/FeaturedSwiper.tsx
+++ app/components/FeaturedSwiper.tsx
@@ -1,8 +1,7 @@
 "use client";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import { Pagination } from "swiper/modules";
 import Link from "next/link";
-import ShowDate from "@/app/components/ShowDate";
 import "swiper/css";
 import "swiper/css/pagination";
 import Image from "next/image";
@@ -20,67 +19,41 @@ export default function FeaturedSwiper({
 	gap: number;
 }) {
 	return (
-		<section className={`${section.bg_colour} relative`}>
-			<Swiper
-				className=""
-				modules={[Pagination]}
-				spaceBetween={gap}
-				pagination={{
-					el: ".swiper-pagination",
-					clickable: true,
-					renderBullet: (index, className) => {
-						return '<span class="' + className + '"></span>';
-					},
-				}}
-				slidesPerView={view}
-				breakpoints={{
-					576: {
-						spaceBetween: 30,
-						slidesPerView: 2,
-					},
-					768: {
-						slidesPerView: 3,
-					},
-					1024: {
-						slidesPerView: 5,
-						spaceBetween: 40,
-					},
-				}}
-			>
-				{featured &&
-					featured instanceof Array &&
-					featured.map((category: Category, index) => {
-						return (
-							<SwiperSlide
-								key={index}
-								className="relative mb-0 mb-md-0 overflow-clip border border-gray-300 dark:border-zinc-800 rounded-t min-h-[200px]"
+		<div
+			className={`${section.bg_colour} relative grid grid-cols-12 gap-3 md:gap-5`}
+		>
+			{featured &&
+				featured instanceof Array &&
+				featured.map((category: Category, index) => {
+					return (
+						<div
+							key={index}
+							className="col-span-4 md:col-span-2 relative mb-0 mb-md-0 overflow-clip border border-gray-300 dark:border-zinc-800 rounded-t md:min-h-[200px]"
+						>
+							<Link
+								href={`${process.env.BASE_URL}/archive?cat=${category.slug}`}
+								className="category relative"
 							>
-								<Link
-									href={`${process.env.BASE_URL}/archive?cat=${category.slug}`}
-									className="category relative"
-								>
-									<Image
-										alt={category.name}
-										width={250}
-										height={250}
-										className="z-50 mb-0 object-cover"
-										src={`${process.env.NEXT_PUBLIC_STRAPI_BASE_URL}${category.image.url}`}
-										priority={false}
-									/>
-								</Link>
-								<SwiperText
-									id={category.id}
-									summary={category.description}
-									path={category.slug}
-									route={`${process.env.BASE_URL}/archive?cat=${category.slug}`}
-									name={category.name}
+								<Image
+									alt={category.name}
+									width={250}
+									height={250}
+									className="z-50 mb-0 object-cover"
+									src={`${process.env.NEXT_PUBLIC_STRAPI_BASE_URL}${category.image.url}`}
+									priority={false}
 								/>
-							</SwiperSlide>
-						);
-					})}
-			</Swiper>
-			<div className="swiper-pagination flex justify-center w-full !-bottom-5 gap-x-2"></div>
-		</section>
+							</Link>
+							<SwiperText
+								id={category.id}
+								summary={category.description}
+								path={category.slug}
+								route={`${process.env.BASE_URL}/archive?cat=${category.slug}`}
+								name={category.name}
+							/>
+						</div>
+					);
+				})}
+		</div>
 	);
 }
 
@@ -98,8 +71,11 @@ function SwiperText({
 	name: string;
 }) {
 	return (
-		<div className="bottom-0 w-full mb-0 flex flex-col px-2 text-center dark:bg-zinc-600 py-1">
-			<Link href={route} className="block link text-base font-bold capitalize">
+		<div className="bottom-0 w-full mb-0 flex flex-col px-1 text-center dark:bg-zinc-600 md:py-1">
+			<Link
+				href={route}
+				className="block link text-sm md:text-base md:font-bold capitalize"
+			>
 				{name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()}
 			</Link>
 			{/*<span className="text-base">{summary}</span>*/}
